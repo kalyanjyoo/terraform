24a934159c9c9e133c0de83447576a415bdc5e73
kalyan <113099699+kalyanjyoo@users.noreply.github.com>
2023-06-23 19:55:27 +0530

    Update eks-cluster.tf

--- assignment-2/eks-cluster.tf
+++ assignment-2/eks-cluster.tf
@@ -2,6 +2,7 @@
 resource "aws_eks_cluster" "eks-cluster" {
   name     = "${local.name}-${var.cluster-name}"
   role_arn = aws_iam_role.eks-master-role.arn
+  version = "${var.cluster-version}"
   
   vpc_config {
     subnet_ids = [
