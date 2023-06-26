e34bb97586290b7fb57e8cf689d6fd429de5f622
kalyan <113099699+kalyanjyoo@users.noreply.github.com>
2023-06-26 11:28:08 +0530

    Update eks-cluster.tf

--- assignment-3/eks-cluster.tf
+++ assignment-3/eks-cluster.tf
@@ -2,12 +2,12 @@
 resource "aws_eks_cluster" "eks-cluster" {
   name     = "${local.name}-${var.cluster-name}"
   role_arn = aws_iam_role.eks-master-role.arn
-  version = "${var.cluster_version}"
+  version = "${var.cluster-version}"
   
   vpc_config {
     subnet_ids = flatten([aws_subnet.public[*].id, aws_subnet.private[*].id])
     endpoint_endpoint_private_access = "${var.cluster-endpoint-private-access}"
-    endpoint_public_access = "${var.cluster-endpoint-public-acess}"
+    endpoint_public_access = "${var.cluster-endpoint-public-access}"
     public_access_cidrs = "${var.cluster-endpoint-public-access-cidrs}"
   }
 
