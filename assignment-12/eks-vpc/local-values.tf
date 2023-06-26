58be72ff6d3770267d1d41fe2a13016791be8cc6
kalyan <113099699+kalyanjyoo@users.noreply.github.com>
2023-06-26 18:50:27 +0530

    Delete assignment-12 directory

--- assignment-12/eks-vpc/local-values.tf
+++
@@ -1,8 +0,0 @@
-# Define Local Values in Terraform
-locals {
-  owners = var.business-divsion
-  environment = var.environment
-  name = "${var.business-divsion}-${var.environment}"
-  #name = "${local.owners}-${local.environment}"
-  eks-cluster-name = "${local.name}-${var.cluster-name}"
-} 
